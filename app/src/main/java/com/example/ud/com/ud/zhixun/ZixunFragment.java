8e7a621744dc3441ac03e73c98e99ca98d4a74ca
YuSir <2040199567@qq.com>
2019-05-20 16:51:53 +0800

    First

---
+++ app/src/main/java/com/example/ud/com/ud/zhixun/ZixunFragment.java
@@ -0,0 +1,20 @@
+package com.example.ud.com.ud.zhixun;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ud.R;
+
+public class ZixunFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.zixun_fragment, container, false);
+        return view;
+    }
+}
